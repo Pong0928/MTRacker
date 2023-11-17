5ab645c27084809b9129710b4157b3a5ebec17a3
Rey <golingping@gmail.com>
2023-11-17 16:54:31 -0500

    update zoom var

--- src/component/SetViewOnClick.tsx
+++ src/component/SetViewOnClick.tsx
@@ -9,7 +9,7 @@ const SetViewOnClick: React.FC<SetViewOnClickProps> = ({ coords }) => {
   const map = useMap();
 
   // Set the view of the map to the specified coordinates when the component is rendered
-  map.setView(coords, map.getZoom());
+  map.setView(coords, 12);
 
   // The component doesn't render anything (returns null)
   return null;
